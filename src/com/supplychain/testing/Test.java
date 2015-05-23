3f20ccf0dc7753032d986c1bd567162a147bfb6f
Ratzzz <saravanan.is16@gmail.com>
2015-05-23 05:35:53 -0400

    First Commit

---
+++ src/com/supplychain/testing/Test.java
@@ -0,0 +1,11 @@
+package com.supplychain.testing;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+		System.out.println( "Welcome to COMP 6231 Project group" );
+	}
+
+}
